e148969543d8da8d571d6990bae3b6d3638c451b
mricarte <mricarte@itba.edu.ar>
2020-09-04 15:45:01 -0300

    <internal>: attempting to use RequestParams...
     to know what study to request later

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -1,10 +1,13 @@
 package ar.edu.itba.paw.webapp.controller;
 
-import ar.edu.itba.paw.interfaces.UserService;
+import ar.edu.itba.paw.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,11 +15,30 @@ public class HelloWorldController {
 
     @Autowired
     private UserService us;
-
+    /*
     @RequestMapping("/")
     public ModelAndView helloWorld() {
         final ModelAndView mav = new ModelAndView("index");
         mav.addObject("user", us.findById(1));
         return mav;
     }
+
+    //url/view-study/asdas
+
+    @RequestMapping("/view-study/{studyId}")
+    public ModelAndView viewStudy(@PathVariable("studyId") final long id) {
+        final ModelAndView mav = new ModelAndView("index");
+        mav.addObject("user", us.findById(id));
+        return mav;
+    }
+    */
+
+    //url/?studyId=asdas
+    @RequestMapping("/")
+    public ModelAndView viewStudy2(@RequestParam(value = "studyId", required = false, defaultValue = "0") final long id) {
+        final ModelAndView mav = new ModelAndView("index");
+        mav.addObject("user", us.findById(id));
+        return mav;
+    }
+    
 }
