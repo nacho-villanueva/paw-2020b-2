5460bf8841acf72b66f84d0122b58cc322674ab8
henry <etawara@itba.edu.ar>
2020-09-06 17:57:14 -0300

    <internal>: basic input for files implemented
    Contains barebone controller, added temporal result-uploaded page to verify the files that were uploaded. working on image display

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -4,6 +4,7 @@ package ar.edu.itba.paw.webapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -27,6 +28,13 @@ public class WebConfig {
 
 
         return viewResolver;
+    }
+
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(1000000);
+        return multipartResolver;
     }
 
 }
---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ResultUploadController.java
@@ -0,0 +1,32 @@
+package ar.edu.itba.paw.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResultUploadController {
+
+
+    @RequestMapping("/create-result")
+    public ModelAndView helloWorld() {
+        final ModelAndView mav = new ModelAndView("create-result");
+        return mav;
+    }
+
+    @RequestMapping(value = "/result-uploaded", method = RequestMethod.POST)
+    public String submit(@RequestParam("files") MultipartFile[] files, @RequestParam("sign") MultipartFile sign, ModelMap modelMap) {
+
+        // TODO: validations for submitted data
+
+        // TODO: upload files to the database in this step
+
+        modelMap.addAttribute("files", files);
+        modelMap.addAttribute("sign", sign);
+        return "result-uploaded";
+    }
+}
