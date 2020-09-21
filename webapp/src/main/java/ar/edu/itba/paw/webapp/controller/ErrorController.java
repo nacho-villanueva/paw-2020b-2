dd95c8bb3cad88aec59c91d7799936c3a1bd1027
Zero <crojas@itba.edu.ar>
2020-09-21 20:26:46 -0300

    Removed 403 jsp, used error jsp instead

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
@@ -1,14 +1,20 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ErrorController {
 
     @RequestMapping("/403")
+    @ResponseStatus(code=HttpStatus.FORBIDDEN)
     public ModelAndView forbidden() {
-        return new ModelAndView("403");
+        final ModelAndView mav = new ModelAndView("error");
+        mav.addObject("errorNumber", HttpStatus.FORBIDDEN.value());
+        mav.addObject("errorText","Youuuuu shall not paaasssssss!\n-Angry Gandalf Noises-");
+        return mav;
     }
 }
