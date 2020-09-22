34af100d0e9b0b254e89c16653279e7a30c434c6
mricarte <mricarte@itba.edu.ar>
2020-09-22 12:09:13 -0300

    <internal>: first attempt at wiring the /home controller

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HomeController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HomeController.java
@@ -1,7 +1,10 @@
 package ar.edu.itba.paw.webapp.controller;
 
 
+import ar.edu.itba.paw.model.Order;
 import ar.edu.itba.paw.model.User;
+import ar.edu.itba.paw.services.OrderService;
+import ar.edu.itba.paw.services.UrlEncoderService;
 import ar.edu.itba.paw.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -11,6 +14,7 @@ import ar.edu.itba.paw.webapp.form.RegisterUserForm;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Collection;
 import java.util.Optional;
 
 @Controller
@@ -18,6 +22,12 @@ public class HomeController {
 
     @Autowired
     private UserService us;
+
+    @Autowired
+    private OrderService os;
+
+    @Autowired
+    private UrlEncoderService urlEncoderService;
 
     @RequestMapping("/")
     public ModelAndView home(@ModelAttribute("registerUserForm") RegisterUserForm registerUserForm,
@@ -32,8 +42,11 @@ public class HomeController {
 
     @RequestMapping("/home")
     public ModelAndView dashboard() {
-        final ModelAndView mav = new ModelAndView("home");
+        ModelAndView mav = new ModelAndView("home");
         mav.addObject("loggedUser",loggedUser());
+
+        mav = homeSetup(mav);
+
         return mav;
     }
 
@@ -44,5 +57,38 @@ public class HomeController {
         //LOGGER.debug("Logged user is {}", user);
         //TODO: see more elegant solution
         return user.orElse(null);
+    }
+
+
+
+    private ModelAndView homeSetup(ModelAndView mav){
+        Collection<Order> patient_studies, medic_studies, clinic_studies;
+        Collection<String> patient_encodeds = null, medic_encodeds = null, clinic_encodeds = null;
+        clinic_studies = os.getAllAsClinic(loggedUser());
+        medic_studies = os.getAllAsMedic(loggedUser());
+        patient_studies = os.getAllAsPatient(loggedUser());
+        patient_encodeds = encoder(patient_studies, patient_encodeds);
+        medic_encodeds = encoder(medic_studies, medic_encodeds);
+        clinic_encodeds = encoder(clinic_studies, clinic_encodeds);
+        mav.addObject("patient_studies", patient_studies);
+        mav.addObject("medic_studies", medic_studies);
+        mav.addObject("clinic_studies", clinic_studies);
+        mav.addObject("patient_encodeds", patient_encodeds);
+        mav.addObject("medic_encodeds", medic_encodeds);
+        mav.addObject("clinic_encodeds", clinic_encodeds);
+        boolean has_studies = false;
+        if(patient_studies.size() + clinic_studies.size() + medic_studies.size() > 0){
+            has_studies = true;
+        }
+        mav.addObject("has_studies", has_studies);
+        return mav;
+
+    }
+
+    private Collection<String> encoder(Collection<Order> orders, Collection<String> encodeds){
+        for(Order order : orders){
+            encodeds.add(urlEncoderService.encode(order.getOrder_id()));
+        }
+        return encodeds;
     }
 }
