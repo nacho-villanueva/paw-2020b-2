677beffb141dccf243e9bc56f14897ca2b41ba40
henry <etawara@itba.edu.ar>
2020-09-22 16:44:23 -0300

    new mail types: sendMedicApplicationValidatingMail & sendClinicApplicationValidatingMail
    -refactor to mail templates

--- services/src/main/java/ar/edu/itba/paw/services/MailNotificationServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/MailNotificationServiceImpl.java
@@ -50,8 +50,7 @@ public class MailNotificationServiceImpl implements MailNotificationService {
     private boolean useHTML;
     private Collection<String> mailInline;
 
-    private String orderTextTemplate;
-    private String resultTextTemplate;
+    private String textTemplate;
 
     @PostConstruct
     private void init(){
@@ -62,8 +61,7 @@ public class MailNotificationServiceImpl implements MailNotificationService {
         this.medicResultSubject     = "[Order <replace-order-id/>-<replace-result-id/>] New results are available.";
         this.clinicResultSubject    = "[Order <replace-order-id/>-<replace-result-id/>] Your results has been uploaded successfully.";
 
-        this.orderTextTemplate    = "Details for this order are available from:\n\n<replace-order-url/>\n\nContact Info\n<replace-contact1-role/>: <replace-contact1-name/> (<replace-contact1-email/>)\n<replace-contact2-role/>: <replace-contact2-name/> (<replace-contact2-email/>)\n\nWith love,\n\tMedTracker (<replace-url/>)";
-        this.resultTextTemplate   = orderTextTemplate;
+        this.textTemplate    = "<replace-content/>\n\nWith love,\n\tMedTracker (<replace-url/>)";
 
 
         try {
@@ -97,7 +95,28 @@ public class MailNotificationServiceImpl implements MailNotificationService {
                     "<table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +
                     "<tr><td><h2> See changes on our site </h2>\n" +
                     "<a href=\"<replace-order-url/>\" style=\"background-color:#009688;border-radius:4px;color:#ffffff;display:inline-block;;font-size:20px;font-weight:normal;line-height:50px;text-align:center;text-decoration:none;width:160px;font-weight:bold\" target=\"_blank\">View Order</a>\n" +
-                    "</td></tr></table>";
+                    "</td></tr></table>\n" +
+                    "<table width=\"440\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\n" +
+                    "                <tr>\n" +
+                    "                    <td>\n" +
+                    "                        <p>\n" +
+                    "                            Contact Info\n" +
+                    "                        </p>\n" +
+                    "                        <p>\n" +
+                    "                            <replace-contact1-role/>: <span style=\"font-weight:bold;\"><replace-contact1-name/></span>\n" +
+                    "                            <a href=\"mailto:<replace-contact1-email/>\" target =\"_blank\" title=\"Send Mail\" style=\"text-decoration: none;\">\n" +
+                    "                                <img height=\"10\" class=\"image_fix\" src=\"cid:envelope-regular.png\" alt=\"(Send Mail)\"/>\n" +
+                    "                            </a>\n" +
+                    "                        </p>\n" +
+                    "                        <p>\n" +
+                    "                            <replace-contact2-role/>: <span style=\"font-weight:bold;\"><replace-contact2-name/></span>\n" +
+                    "                            <a href=\"mailto:<replace-contact2-email/>\" target =\"_blank\" title=\"Send Mail\" style=\"text-decoration: none;\">\n" +
+                    "                                <img height=\"10\" class=\"image_fix\" src=\"cid:envelope-regular.png\" alt=\"(Send Mail)\"/>\n" +
+                    "                            </a>\n" +
+                    "                        </p>\n" +
+                    "                    </td>\n" +
+                    "                </tr>\n" +
+                    "            </table>";
 
             basicMailContent = basicMailContent.replace("<replace-content/>",body);
 
@@ -146,7 +165,11 @@ public class MailNotificationServiceImpl implements MailNotificationService {
             );
 
         }else{
-            String basicMailContent = replaceURL(this.orderTextTemplate);
+
+            String body = "Details for this order are available from:\n\n<replace-order-url/>\n\nContact Info\n<replace-contact1-role/>: <replace-contact1-name/> (<replace-contact1-email/>)\n<replace-contact2-role/>: <replace-contact2-name/> (<replace-contact2-email/>)";
+
+            String basicMailContent = replaceURL(getTextTemplate());
+            basicMailContent = basicMailContent.replaceAll("<replace-content/>",body);
             String mailContent;
 
             // mail to patient
@@ -209,11 +232,30 @@ public class MailNotificationServiceImpl implements MailNotificationService {
 
                 String body =
                         "<table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +
-                                "<h2>\n" +
-                                "See changes on our site\n" +
-                                "</h2>\n" +
+                                "<tr><td><h2> See changes on our site </h2>\n" +
                                 "<a href=\"<replace-order-url/>\" style=\"background-color:#009688;border-radius:4px;color:#ffffff;display:inline-block;;font-size:20px;font-weight:normal;line-height:50px;text-align:center;text-decoration:none;width:160px;font-weight:bold\" target=\"_blank\">View Order</a>\n" +
-                                "</table>";
+                                "</td></tr></table>\n" +
+                                "<table width=\"440\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\n" +
+                                "                <tr>\n" +
+                                "                    <td>\n" +
+                                "                        <p>\n" +
+                                "                            Contact Info\n" +
+                                "                        </p>\n" +
+                                "                        <p>\n" +
+                                "                            <replace-contact1-role/>: <span style=\"font-weight:bold;\"><replace-contact1-name/></span>\n" +
+                                "                            <a href=\"mailto:<replace-contact1-email/>\" target =\"_blank\" title=\"Send Mail\" style=\"text-decoration: none;\">\n" +
+                                "                                <img height=\"10\" class=\"image_fix\" src=\"cid:envelope-regular.png\" alt=\"(Send Mail)\"/>\n" +
+                                "                            </a>\n" +
+                                "                        </p>\n" +
+                                "                        <p>\n" +
+                                "                            <replace-contact2-role/>: <span style=\"font-weight:bold;\"><replace-contact2-name/></span>\n" +
+                                "                            <a href=\"mailto:<replace-contact2-email/>\" target =\"_blank\" title=\"Send Mail\" style=\"text-decoration: none;\">\n" +
+                                "                                <img height=\"10\" class=\"image_fix\" src=\"cid:envelope-regular.png\" alt=\"(Send Mail)\"/>\n" +
+                                "                            </a>\n" +
+                                "                        </p>\n" +
+                                "                    </td>\n" +
+                                "                </tr>\n" +
+                                "            </table>";
 
                 basicMailContent = basicMailContent.replace("<replace-content/>",body);
 
@@ -262,7 +304,11 @@ public class MailNotificationServiceImpl implements MailNotificationService {
                 );
 
             }else{
-                String basicMailContent = replaceURL(this.resultTextTemplate);
+
+                String body = "Details for this order are available from:\n\n<replace-order-url/>\n\nContact Info\n<replace-contact1-role/>: <replace-contact1-name/> (<replace-contact1-email/>)\n<replace-contact2-role/>: <replace-contact2-name/> (<replace-contact2-email/>)";
+
+                String basicMailContent = replaceURL(getTextTemplate());
+                basicMailContent = basicMailContent.replaceAll("<replace-content/>",body);
                 String mailContent;
 
                 // mail to patient
@@ -307,11 +353,84 @@ public class MailNotificationServiceImpl implements MailNotificationService {
         }
     }
 
+    public void sendMedicApplicationValidatingMail(Medic medic){
+        String medicMail   = medic.getEmail();
+
+        String mailSubject = "Processing Medic Application form for <replace-medic-name/>";
+
+        if(this.useHTML){
+            String basicMailContent = getMailTemplate();
+
+            String body = "<table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +
+                    "<tr><td><h2> We are now processing your Medic Application Form\n" +
+                    "<p>We will notify you when the validation process finishes</p>\n"+
+                    "</td></tr></table>";
+
+            String mailContent = basicMailContent.replace("<replace-content/>",body);
+
+            ms.sendMimeMessage(medicMail,
+                    replaceMedicInfo(mailSubject, medic),
+                    replaceMedicInfo(mailContent, medic),
+                    this.mailInline
+                    );
+        }else{
+            String basicMailContent = getTextTemplate();
+
+            String body = "We are now processing your Medic Application Form.\nWe will notify you after the validation process ends.";
+
+            String mailContent = basicMailContent.replace("<replace-content/>",body);
+
+            ms.sendSimpleMessage(medicMail,
+                    replaceMedicInfo(mailSubject, medic),
+                    replaceMedicInfo(mailContent, medic)
+            );
+        }
+
+    }
+
+    public void sendClinicApplicationValidatingMail(Clinic clinic){
+        String clinicMail   = clinic.getEmail();
+
+        String mailSubject = "Processing Medic Application form for <replace-clinic-name/>";
+
+        if(this.useHTML){
+            String basicMailContent = getMailTemplate();
+
+            String body = "<table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +
+                    "<tr><td><h2> We are now processing your Clinic Application Form\n" +
+                    "<p>We will notify you when the validation process finishes</p>\n"+
+                    "</td></tr></table>";
+
+            String mailContent = basicMailContent.replace("<replace-content/>",body);
+
+            ms.sendMimeMessage(clinicMail,
+                    replaceClinicInfo(mailSubject, clinic),
+                    replaceClinicInfo(mailContent, clinic),
+                    this.mailInline
+            );
+        }else{
+            String basicMailContent = getTextTemplate();
+
+            String body = "We are now processing your Clinic Application Form.\nWe will notify you after the validation process ends.";
+
+            String mailContent = basicMailContent.replace("<replace-content/>",body);
+
+            ms.sendSimpleMessage(clinicMail,
+                    replaceClinicInfo(mailSubject, clinic),
+                    replaceClinicInfo(mailContent, clinic)
+            );
+        }
+
+    }
     // replace functions
     private String getMailTemplate(){
         String mailTemplateString = this.mailTemplate.lines().collect(Collectors.joining("\n"));
         mailTemplateString = replaceURL(mailTemplateString);
         return mailTemplateString;
+    }
+
+    private String getTextTemplate(){
+        return this.textTemplate;
     }
 
     private String replaceURL(String mail){
