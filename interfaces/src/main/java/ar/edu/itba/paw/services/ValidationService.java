2e8a39a41b9447c940f281d0a9dbedc2b0474d3c
Zero <crojas@itba.edu.ar>
2020-09-22 02:28:11 -0300

    Implemented ValidationService

---
+++ interfaces/src/main/java/ar/edu/itba/paw/services/ValidationService.java
@@ -0,0 +1,45 @@
+package ar.edu.itba.paw.services;
+
+import ar.edu.itba.paw.model.*;
+
+import java.sql.Date;
+
+public interface ValidationService {
+    boolean isValid(User user);
+
+    boolean isValid(Medic medic);
+
+    boolean isValid(Order order);
+
+    boolean isValid(Patient patient);
+
+    boolean isValid(Result result);
+
+    boolean isValid(MedicalField medicalField);
+
+    boolean isValid(StudyType studyType);
+
+    boolean isValid(Clinic clinic);
+
+    boolean isValidEmail(String email);
+
+    boolean isValidIdentification_Type(String identification_type);
+
+    boolean isValidTelephone(String telephone);
+
+    boolean isValidRole(int role);
+
+    boolean isValidName(String name);
+
+    boolean isValidPassword(String password);
+
+    boolean isValidLicenceNumber(String licence_number);
+
+    boolean isValidMedicPlan(String medic_plan);
+
+    boolean isValidMedicPlanNumber(String medic_plan_number);
+
+    boolean isValidResultDataType(String result_data_type);
+
+    boolean isValidDate(Date date);
+}
