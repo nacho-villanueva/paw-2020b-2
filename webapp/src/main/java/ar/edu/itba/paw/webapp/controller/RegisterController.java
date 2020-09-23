54f40aa3e0a2cf8793dfbeb51ceab9452da7a280
Nacho Villanueva <ivillanueva@itba.edu.ar>
2020-09-23 03:05:37 -0300

    Added dependencies + Homepage registration form validation

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/RegisterController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/RegisterController.java
@@ -5,10 +5,13 @@ import ar.edu.itba.paw.services.UserService;
 import ar.edu.itba.paw.webapp.form.RegisterUserForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class RegisterController {
@@ -17,7 +20,13 @@ public class RegisterController {
     private UserService us;
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ModelAndView register(@ModelAttribute("registerUserForm") RegisterUserForm registerUserForm){
+    public ModelAndView register(@Valid @ModelAttribute("registerUserForm") RegisterUserForm registerUserForm, final BindingResult errors){
+        if(errors.hasErrors()){
+            ModelAndView registrationErrors = new ModelAndView("index");
+            registrationErrors.addObject("registration", true);
+            return registrationErrors;
+        }
+
         ModelAndView mav = new ModelAndView("redirect:/?registrationSuccess=true");
 
         User newUser = us.register(registerUserForm.getEmail(),registerUserForm.getPassword());
@@ -25,4 +34,5 @@ public class RegisterController {
         //TODO mav.addObject(loginUserForm);
         return mav;
     }
+
 }
