c749ee81008055283e7c3b30ff7c9671a46f7ca6
mricarte <mricarte@itba.edu.ar>
2020-11-09 15:22:20 -0300

    <fix>: fixed select not showing newly added options

--- webapp/src/main/webapp/resources/js/addOption.js
+++ webapp/src/main/webapp/resources/js/addOption.js
@@ -3,5 +3,6 @@ function addOption(mySelectId,newOptionValue) {
     if(newOptionValue !== ""){
         let newOption = sanitize(newOptionValue);
         $(mySelectId).append('<option value="'+newOption+'" selected="">'+newOption+'</option>');
+        $(mySelectId).selectpicker("refresh");
     }
 }
